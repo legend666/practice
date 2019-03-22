05ad893271e6bdacae82532119b4206b4a1b672d
Based-on-practice <792066658@qq.com>
2019-03-22 10:53:40 +0800

    basics

--- ClassicTopic/DynamicProgarmming(dp).py
+++ ClassicTopic/DynamicProgarmming(dp).py
@@ -13,8 +13,8 @@
 # currency_list 为该国货币的面值列表，
 # num_list 为需要找零的最少货币数目, num_list的长度至少为(need_change+1)
 # used_list 为需要找零的最少货币数目, 长度与num_list相同
-class Change(object):
-    def giveChange(self,need_change, currency_list, num_list, used_list):
-        for change in range(need_change + 1):
-            for currency in currency_list:
-                if (change - currency >= 0) and （看看在敲吧）
+# class Change(object):
+    # def giveChange(self,need_change, currency_list, num_list, used_list):
+        # for change in range(need_change + 1):
+            # for currency in currency_list:
+                # if (change - currency >= 0) and #（看看在敲吧）
---
+++ FluentPython/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/22 10:51
+# @Author  : legend
