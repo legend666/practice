357f010336e169dc0b6f5b78e9c3567aa015951d
Based-on-practice <792066658@qq.com>
2019-05-15 16:16:02 +0800

    20190515

---
+++ GeekTime/PythonCoreTechnologiesAndPractices/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/15 16:07
+# @Author  : legend
---
+++ basics/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/13 9:26
+# @Author  : legend
